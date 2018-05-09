557243a6269eafa7c7eae638e136dd81aa2e015a
Alberto PF <albertoperezfraile@gmail.com>
2018-05-09 09:12:09 +0200

    hola de nuevo un commit de comprobacion

---
+++ Alvaro/src/holamundo.java
@@ -0,0 +1,7 @@
+
+public class holamundo {
+	public static void main(String[] arguments) {
+        String greeting = "hola mundo!";
+        System.out.println(greeting);
+    }
+}
---
+++ Alvaro/src/unsaludo.java
@@ -0,0 +1,7 @@
+
+public class unsaludo {
+	public static void main(String[] arguments) {
+        String greeting = "un saludo mas";
+        System.out.println(greeting);
+    }
+}
